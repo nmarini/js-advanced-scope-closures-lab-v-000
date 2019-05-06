e75e8757c5a193bb88619d574fb85b73e03aee19
Nicolas Marini <nmarini44@gmail.com>
2019-05-06 21:08:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function produceDrivingRange(blockRange){
+  return function(startBlock, endBlock){
+    const startRange = parseInt(blockRange) + startNum.slice(0, -2)
+    if ((startRange - endBlock))
+  }
+}
