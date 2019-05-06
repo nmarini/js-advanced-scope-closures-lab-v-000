3d84ebf0d73d57353e53d948a1b004c1f3916718
Nicolas Marini <nmarini44@gmail.com>
2019-05-06 21:12:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function produceDrivingRange(blockRange){
   return function(startBlock, endBlock){
-    const startRange = parseInt(blockRange) + startNum.slice(0, -2)
-    if ((startRange - endBlock))
+    const startRange = parseInt(blockRange) + parseInt(startNum.slice(0, -2))
+    const result = startRange - parseInt(endBlock)
+    if (result >= 0){
+      return `within range by ${result}`
+    else 
+      return `${result} blocks out of range`
+    }
   }
 }
